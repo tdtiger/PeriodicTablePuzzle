770a8e7f8571ef80cfa260e4395816c758777793
鯛 <pf529crm@s.okayama-u.ac.jp>
2025-10-21 18:16:32 +0900

    日本語名の追加と、正確な位置に移動したときに色が変わる機能を実装

--- sketch.js
+++ sketch.js
@@ -7,23 +7,24 @@ const COLS = 18;
 const ROWS = 7;
 let emptyCol = 0;
 let emptyRow = 0;
-let tileSize = 60;
+let tileSize = 65;
 
 class Element{
-    constructor(name, num){
+    constructor(name, num, name_ja){
         this.name = name;
         this.num = num;
+        this.name_ja = name_ja;
     }
 }
 
 const TABLE =[
-    [new Element("H", 1), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("He", 2)],
-    [new Element("Li", 3), new Element("Be", 4), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("B", 5), new Element("C", 6), new Element("N", 7), new Element("O", 8), new Element("F", 9), new Element("Ne", 10)],
-    [new Element("Na", 11), new Element("Mg", 12), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("", ), new Element("Al", 13), new Element("Si", 14), new Element("P", 15), new Element("S", 16), new Element("Cl", 17), new Element("Ar", 18)],
-    [new Element("K", 19), new Element("Ca", 20), new Element("Sc", 21), new Element("Ti", 22), new Element("V", 23), new Element("Cr", 24), new Element("Mn", 25), new Element("Fe", 26), new Element("Co", 27), new Element("Ni", 28), new Element("Cu", 29), new Element("Zn", 30), new Element("Ga", 31), new Element("Ge", 32), new Element("As", 33), new Element("Se", 34), new Element("Br", 35), new Element("Kr", 36)],
-    [new Element("Rb", 37), new Element("Sr", 38), new Element("Y", 39), new Element("Zr", 40), new Element("Nb", 41), new Element("Mo", 42), new Element("Tc", 43), new Element("Ru", 44), new Element("Rh", 45), new Element("Pd", 46), new Element("Ag", 47), new Element("Cd", 48), new Element("In", 49), new Element("Sn", 50), new Element("Sb", 51), new Element("Te", 52), new Element("I", 53), new Element("Xe", 54)],
-    [new Element("Cs", 55), new Element("Ba", 56), new Element("La", 57), new Element("Hf", 72), new Element("Ta", 73), new Element("W", 74), new Element("Re", 75), new Element("Os", 76), new Element("Ir", 77), new Element("Pt", 78), new Element("Au", 79), new Element("Hg", 80), new Element("Tl", 81), new Element("Pb", 82), new Element("Bi", 83), new Element("Po", 84), new Element("At", 85), new Element("Rn", 86)],
-    [new Element("Fr", 87), new Element("Ra", 88), new Element("Ac", 89), new Element("Rf", 104), new Element("Db", 105), new Element("Sg", 106), new Element("Bh", 107), new Element("Hs", 108), new Element("Mt", 109), new Element("Ds", 110), new Element("Rg", 111), new Element("Cn", 112), new Element("Nh", 113), new Element("Fl", 114), new Element("Mc", 115), new Element("Lv", 116), new Element("Ts", 117), new Element("Og", 118)],
+    [new Element("H", 1, "水素"), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("He", 2, "ヘリウム")],
+    [new Element("Li", 3, "リチウム"), new Element("Be", 4, "ベリリウム"), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("B", 5, "ホウ素"), new Element("C", 6, "炭素"), new Element("N", 7, "窒素"), new Element("O", 8, "酸素"), new Element("F", 9, "フッ素"), new Element("Ne", 10, "ネオン")],
+    [new Element("Na", 11, "ナトリウム"), new Element("Mg", 12, "マグネシウム"), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("Al", 13, "アルミニウム"), new Element("Si", 14, "ケイ素"), new Element("P", 15, "リン"), new Element("S", 16, "硫黄"), new Element("Cl", 17, "塩素"), new Element("Ar", 18, "アルゴン")],
+    [new Element("K", 19, "カリウム"), new Element("Ca", 20, "カルシウム"), new Element("Sc", 21, "スカンジウム"), new Element("Ti", 22, "チタン"), new Element("V", 23, "バナジウム"), new Element("Cr", 24, "クロム"), new Element("Mn", 25, "マンガン"), new Element("Fe", 26, "鉄"), new Element("Co", 27, "コバルト"), new Element("Ni", 28, "ニッケル"), new Element("Cu", 29, "銅"), new Element("Zn", 30, "亜鉛"), new Element("Ga", 31, "ガリウム"), new Element("Ge", 32, "ゲルマニウム"), new Element("As", 33, "ヒ素"), new Element("Se", 34, "セレン"), new Element("Br", 35, "臭素"), new Element("Kr", 36, "クリプトン")],
+    [new Element("Rb", 37, "ルビジウム"), new Element("Sr", 38, "ストロンチウム"), new Element("Y", 39, "イットリウム"), new Element("Zr", 40, "ジルコニウム"), new Element("Nb", 41, "ニオブ"), new Element("Mo", 42, "モリブデン"), new Element("Tc", 43, "テクネチウム"), new Element("Ru", 44, "ルテニウム"), new Element("Rh", 45, "ロジウム"), new Element("Pd", 46, "パラジウム"), new Element("Ag", 47, "銀"), new Element("Cd", 48, "カドミウム"), new Element("In", 49, "インジウム"), new Element("Sn", 50, "スズ"), new Element("Sb", 51, "アンチモン"), new Element("Te", 52, "テルル"), new Element("I", 53, "ヨウ素"), new Element("Xe", 54, "キセノン")],
+    [new Element("Cs", 55, "セシウム"), new Element("Ba", 56, "バリウム"), new Element("La", 57, "ランタン"), new Element("Hf", 72, "ハフニウム"), new Element("Ta", 73, "タンタル"), new Element("W", 74, "タングステン"), new Element("Re", 75, "レニウム"), new Element("Os", 76, "オスミウム"), new Element("Ir", 77, "イリジウム"), new Element("Pt", 78, "白金"), new Element("Au", 79, "金"), new Element("Hg", 80, "水銀"), new Element("Tl", 81, "タリウム"), new Element("Pb", 82, "鉛"), new Element("Bi", 83, "ビスマス"), new Element("Po", 84, "ポロニウム"), new Element("At", 85, "アスタチン"), new Element("Rn", 86, "ラドン")],
+    [new Element("Fr", 87, "フランジウム"), new Element("Ra", 88, "ラジウム"), new Element("Ac", 89, "アクチニウム"), new Element("Rf", 104, "ラザホージウム"), new Element("Db", 105), new Element("Sg", 106, "シーボーギウム"), new Element("Bh", 107, "ボーリウム"), new Element("Hs", 108, "ハッシウム"), new Element("Mt", 109, "マイトネリウム"), new Element("Ds", 110, "ダームスタチウム"), new Element("Rg", 111, "レントゲニウム"), new Element("Cn", 112, "コペルニシウム"), new Element("Nh", 113, "ニホニウム"), new Element("Fl", 114, "フレロビウム"), new Element("Mc", 115, "モスコビウム"), new Element("Lv", 116, "リバボリウム"), new Element("Ts", 117, "テネシン"), new Element("Og", 118, "オガネソン")],
 ];
 
 function Shuffle2DArray(arr){
@@ -77,19 +78,27 @@ function draw(){
         for(let j = 0; j < COLS; j++){
             let name = question[i][j].name;
             let num = question[i][j].num;
+            let name_ja = question[i][j].name_ja;
 
             let x = j * tileSize;
             let y = i * tileSize;
 
             if(!(i == emptyRow && j == emptyCol)){
-                fill(255);
+                if(name === board[i][j].name)
+                    fill(150, 255, 150);
+                else
+                    fill(255);
                 stroke(0);
                 rect(x, y, tileSize, tileSize);
 
                 fill(0);
                 textAlign(CENTER, CENTER);
+                textSize(10)
                 text(num, x + tileSize / 6, y + tileSize / 6);
+                textSize(25)
                 text(name, x + tileSize / 2, y + tileSize / 2);
+                textSize(9)
+                text(name_ja, x + tileSize / 2, y + tileSize - 10);
             }
         }
     }
