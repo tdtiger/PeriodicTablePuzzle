25a822372a1caa35436e4958729b555059821035
鯛 <pf529crm@s.okayama-u.ac.jp>
2025-10-22 00:10:15 +0900

    リセットボタン、タイマー、セーブの追加

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ let emptyCol = 0;
 let emptyRow = 0;
 let tileSize = 65;
 
+// 各元素を表すクラス
 class Element{
     constructor(name, num, name_ja){
         this.name = name;
@@ -17,6 +18,7 @@ class Element{
     }
 }
 
+// 周期表
 const TABLE =[
     [new Element("H", 1, "水素"), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("He", 2, "ヘリウム")],
     [new Element("Li", 3, "リチウム"), new Element("Be", 4, "ベリリウム"), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("", "", ""), new Element("B", 5, "ホウ素"), new Element("C", 6, "炭素"), new Element("N", 7, "窒素"), new Element("O", 8, "酸素"), new Element("F", 9, "フッ素"), new Element("Ne", 10, "ネオン")],
@@ -27,10 +29,15 @@ const TABLE =[
     [new Element("Fr", 87, "フランジウム"), new Element("Ra", 88, "ラジウム"), new Element("Ac", 89, "アクチニウム"), new Element("Rf", 104, "ラザホージウム"), new Element("Db", 105), new Element("Sg", 106, "シーボーギウム"), new Element("Bh", 107, "ボーリウム"), new Element("Hs", 108, "ハッシウム"), new Element("Mt", 109, "マイトネリウム"), new Element("Ds", 110, "ダームスタチウム"), new Element("Rg", 111, "レントゲニウム"), new Element("Cn", 112, "コペルニシウム"), new Element("Nh", 113, "ニホニウム"), new Element("Fl", 114, "フレロビウム"), new Element("Mc", 115, "モスコビウム"), new Element("Lv", 116, "リバボリウム"), new Element("Ts", 117, "テネシン"), new Element("Og", 118, "オガネソン")],
 ];
 
+let startTime;
+
+// 二次元配列の要素をシャッフルする関数
 function Shuffle2DArray(arr){
+    // 一旦一次元配列にする
     let flatArray = arr.flat();
     let shuffledArray = flatArray.sort(() => Math.random() - 0.5);
 
+    // 二次元配列に戻す
     let result = [];
     let index = 0;
     for(let i = 0; i < arr.length; i++){
@@ -46,28 +53,83 @@ function Shuffle2DArray(arr){
 function Check(){
     for(let i = 0; i < ROWS; i++){
         for(let j = 0; j < COLS; j++){
-            if(question[i][j] !== board[i][j])
+            if(i == emptyRow && j == emptyCol)
+                continue;
+
+            if(question[i][j].name !== board[i][j].name)
                 return false;
         }
     }
     return true;
 }
 
-function setup(){
-    createCanvas(COLS * tileSize, ROWS * tileSize);
+function DisplayTimer(){
+    let passed = millis() - startTime;
 
-    let currentNum = 1;
+    let totalSeconds = floor(passed / 1000);
+
+    let hours = floor(totalSeconds / 3600);
+    let minutes = floor((totalSeconds % 3600) / 60);
+    let seconds = totalSeconds % 60;
+
+    let timeString = hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0');
+
+    fill(0);
+    noStroke();
+    textSize(20);
+    textAlign(LEFT, TOP);
+    text(timeString, 10, 5);
+}
+
+function LoadGame(){
+    let saveData = localStorage.getItem('periodicPuzzleSave');
+
+    if(saveData){
+        let data = JSON.parse(saveData);
+        question = data.question;
+        emptyRow = data.emptyRow;
+        emptyCol = data.emptyCol;
+        startTime = millis() - data.passed;
+    }
+    else{
+        question = Shuffle2DArray(board);
+        emptyRow = 6;
+        emptyCol = 17;
+        startTime = millis();
+        SaveGame();
+    }
+}
+
+function SaveGame(){
+    let currentPassed = millis() - startTime;
+
+    let saveData = {question: question, emptyRow: emptyRow, emptyCol: emptyCol, passed: currentPassed};
+
+    localStorage.setItem('periodicPuzzleSave', JSON.stringify(saveData));
+}
+
+function ResetGame(){
+    question = Shuffle2DArray(board);
+    emptyRow = 6;
+    emptyCol = 17;
+    startTime = millis();
+    SaveGame();
+}
+
+function setup(){
+    createCanvas(COLS * tileSize, ROWS * tileSize + 30);
+
+    let resetButton = createButton('盤面リセット');
+    resetButton.position(180, 70);
+    resetButton.mousePressed(ResetGame);
+
     for(let i = 0; i < ROWS; i++){
         board[i] = [];
-        for(let j = 0; j < COLS; j++){
+        for(let j = 0; j < COLS; j++)
             board[i][j] = TABLE[i][j];
-            currentNum += 1;
-        }
     }
 
-    question = Shuffle2DArray(board);
-    emptyCol = 17;
-    emptyRow = 6;
+    LoadGame();
 }
 
 function draw(){
@@ -81,13 +143,14 @@ function draw(){
             let name_ja = question[i][j].name_ja;
 
             let x = j * tileSize;
-            let y = i * tileSize;
+            let y = i * tileSize + 30;
 
             if(!(i == emptyRow && j == emptyCol)){
                 if(name === board[i][j].name)
                     fill(150, 255, 150);
                 else
                     fill(255);
+
                 stroke(0);
                 rect(x, y, tileSize, tileSize);
 
@@ -102,11 +165,13 @@ function draw(){
             }
         }
     }
+
+    DisplayTimer();
 }
 
 function mousePressed(){
     let clickedCol = floor(mouseX / tileSize);
-    let clickedRow = floor(mouseY / tileSize);
+    let clickedRow = floor((mouseY - 30) / tileSize);
 
     if(clickedCol < 0 || clickedCol >= COLS || clickedRow < 0 || clickedRow >= ROWS)
         return;
@@ -122,8 +187,13 @@ function mousePressed(){
 
         emptyCol = clickedCol;
         emptyRow = clickedRow;
+
+        SaveGame();
     }
 
-    if(Check())
+    if(Check()){
+        // 空きマスに欠落していた元素を当てはめる
+        question[emptyRow][emptyCol] = board[emptyRow][emptyCol];
         window.alert("CLEAR!!");
+    }
 }
